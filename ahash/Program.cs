004911c20161d4b79ec1da1f87fb35e58a122ecf
wuyuansushen <43259764+wuyuansushen@users.noreply.github.com>
2022-03-25 00:03:03 +0800

    Add git repository

---
+++ ahash/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime;
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ahash
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.Write($"Input torrent hash:");
+            string inputHash = ((Console.ReadLine()) ?? throw new ArgumentNullException());
+            try
+            {
+                var aria2cProcess = Process.Start("/usr/bin/aria2c", "--bt-save-metadata=true --bt-metadata-only=true magnet:?xt=urn:btih:" + inputHash);
+                aria2cProcess.WaitForExit();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine($"Invalid hash value.");
+            }
+        }
+    }
+}
